39f6ce1444fd5dabcb9021386f1aeb4e8bac175b
nanapereira <inajara.souza.pereira@gmail.com>
2020-05-14 14:14:15 -0300

    Adicionando combobox

--- src/main/webapp/app/entities/ausencia/ausencia.tsx
+++ src/main/webapp/app/entities/ausencia/ausencia.tsx
@@ -30,6 +30,17 @@ export const Ausencia = (props: IAusenciaProps) => {
           <Translate contentKey="oGestorApp.ausencia.home.createLabel">Create new Ausencia</Translate>
         </Link>
       </h2>
+      <div className="input-group">
+        <select className="custom-select" id="inputGroupSelect04">
+          <option selected>Selecione...</option>
+          <option value="1">One</option>
+          <option value="2">Two</option>
+          <option value="3">Three</option>
+        </select>
+        <div className="input-group-append">
+          <button className="btn btn-outline-secondary" type="button">Buscar</button>
+        </div>
+      </div>
       <div className="table-responsive">
         {ausenciaList && ausenciaList.length > 0 ? (
           <Table responsive>
